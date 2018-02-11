8a1015268f0dc8b6d4a3cfe42db17d3de3e34d37
hamiinimii <Ryuk@Victoria-MacBook-Pro.local>
2018-02-11 22:20:55 +0900

    4.12. unsolved

--- 4.2.GitHub/4.13-CurrencyStyle.py
+++ 4.2.GitHub/4.13-CurrencyStyle.py
@@ -1,5 +1,7 @@
 #checkio_CurrencyStyle
 
+#unsolved
+
 import re
 
 def checkio(text):
@@ -7,7 +9,11 @@ def checkio(text):
     for i,x in enumerate(strlist):
         if (x[:-1].find(',') if x[:-1].rfind(',')>-1 else len(x))>x[:-1].rfind('.'):
             x = re.sub(',', 'C', x)
-            x = re.sub('\.', ',', x) if re.match(x[x.rfind('.'):])
+            print(x)
+            print(bool(re.fullmatch('[0-9][0-9][0-9][C]*',x[x.rfind('.')+1:])))
+            x = re.sub('\.', ',', x) if (re.fullmatch('[0-9][0-9][0-9][C]*',x[x.rfind('.')+1:]) and len(x[x.rfind('.')+1:])<4)\
+            else re.sub('\.', ',', x[:x.rfind('.')])+x[x.rfind('.'):]
+            print(x)
             x = re.sub('C', '.', x)
             x = x[:-1] + ',' if x[-1] == '.' else x
             strlist[i] = x
@@ -26,3 +32,5 @@ if __name__ == '__main__':
            "Us Style = $12,345.67, Euro Style = $12,345.67", "US and European"
     assert checkio("$1.234, $5.678 and $9") == \
            "$1,234, $5,678 and $9", "Dollars without cents"
+    assert checkio("$5.34") == \
+           "$5.34", "Dollars with cents"
